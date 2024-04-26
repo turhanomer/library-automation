8639a51062dae3c64ae04e2f02426d08f56eceae
Omer Turhan <omerturhan16@outlook.com>
2024-04-26 22:30:32 +0300

    Login screen made

--- obj/Debug/net8.0-windows/library-automation.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/library-automation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("library-automation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b08cc452d4ffad1fe7876941f171849e9ee7b2a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51afe488464984b2c6fb1bb0202427a156c50216")]
 [assembly: System.Reflection.AssemblyProductAttribute("library-automation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("library-automation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
